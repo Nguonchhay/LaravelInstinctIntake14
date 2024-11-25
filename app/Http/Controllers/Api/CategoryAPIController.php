b7ef5e70e31c82c257fa51571a3d1522a70ac591
Nguonchhay TOUCH <nguonchhay.touch@gmail.com>
2024-11-25 19:06:45 +0700

    API category crud

--- app/Http/Controllers/Api/CategoryAPIController.php
+++ app/Http/Controllers/Api/CategoryAPIController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Category;
 use App\Http\Resources\CategoryResource;
 
@@ -17,5 +18,43 @@ class CategoryAPIController extends Controller
     public function show(Category $category) {
         // return response()->json($category);
         return new CategoryResource($category);
+    }
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|unique:categories|min:3|max:50'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'errorCode' => '004',
+                'message' => 'Missing required fields' // 'errors.required_field'
+            ]);
+        }
+
+        $category = Category::create($request->all());
+        return new CategoryResource($category);
+    }
+
+    public function update(Category $category, Request $request) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|unique:categories|min:3|max:50'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'errorCode' => '004',
+                'message' => 'Missing required fields' // 'errors.required_field'
+            ]);
+        }
+
+        $category->title = $request->get('title');
+        $category->save();
+        return new CategoryResource($category);
+    }
+
+    public function destroy(Category $category, Request $request) {
+        $category->delete();
+        return response()->json([
+            'message' => 'Category is deleted'
+        ]);
     }
 }
